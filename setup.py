b8f71f06a823057949e9b40c5c063dfce0b11b13
Sven Kreiss <me@svenkreiss.com>
2019-03-19 13:12:37 +0100

    automatically install numpy; make cython optional (#9)

--- setup.py
+++ setup.py
@@ -1,14 +1,41 @@
 from setuptools import setup
 from setuptools.extension import Extension
 
-import numpy
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    cythonize = None
+try:
+    import numpy
+except ImportError:
+    numpy = None
 
 
 # extract version from __init__.py
 with open('openpifpaf/__init__.py', 'r') as f:
     VERSION_LINE = [l for l in f if l.startswith('__version__')][0]
     VERSION = VERSION_LINE.split('=')[1].strip()[1:-1]
+
+
+class NumpyIncludePath(object):
+    """Lazy import of numpy to get include path."""
+    @staticmethod
+    def __str__():
+        import numpy
+        return numpy.get_include()
+
+
+if cythonize is not None and numpy is not None:
+    EXTENSIONS = cythonize([Extension('openpifpaf.functional',
+                                      ['openpifpaf/functional.pyx'],
+                                      include_dirs=[numpy.get_include()]),
+                           ],
+                           annotate=True,
+                           compiler_directives={'language_level': 3})
+else:
+    EXTENSIONS = [Extension('openpifpaf.functional',
+                            ['openpifpaf/functional.pyx'],
+                            include_dirs=[NumpyIncludePath()])]
 
 
 setup(
@@ -27,11 +54,7 @@ setup(
     author='Sven Kreiss',
     author_email='research@svenkreiss.com',
     url='https://github.com/vita-epfl/openpifpaf',
-    ext_modules=cythonize(Extension('openpifpaf.functional',
-                                    ['openpifpaf/functional.pyx'],
-                                    include_dirs=[numpy.get_include()]),
-                          annotate=True,
-                          compiler_directives={'language_level': 3}),
+    ext_modules=EXTENSIONS,
     zip_safe=False,
 
     install_requires=[
