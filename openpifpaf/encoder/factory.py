c6aa57c45c70d8d08e55690ff38055ef66d035dd
Sven Kreiss <me@svenkreiss.com>
2019-04-07 01:12:34 +0200

    move encoder utils into rescaler (#28)

--- openpifpaf/encoder/factory.py
+++ openpifpaf/encoder/factory.py
@@ -1,7 +1,8 @@
+import numpy as np
+
 from .paf import Paf
 from .pif import Pif
 from .skeleton import Skeleton
-from .utils import anns_to_bg_mask, anns_to_keypoint_sets
 from ..data import COCO_PERSON_SKELETON, DENSER_COCO_PERSON_SKELETON, KINEMATIC_TREE_SKELETON
 
 
@@ -58,15 +59,16 @@ def factory(args, io_scales):
 
 
 class AnnRescaler(object):
-    def __init__(self, input_output_scale):
+    def __init__(self, input_output_scale, n_keypoints=17):
         self.input_output_scale = input_output_scale
+        self.n_keypoints = n_keypoints
 
     def __call__(self, anns, width_height_original):
-        keypoint_sets = anns_to_keypoint_sets(anns)
+        keypoint_sets = self.anns_to_keypoint_sets(anns)
         keypoint_sets[:, :, :2] /= self.input_output_scale
 
         # background mask
-        bg_mask = anns_to_bg_mask(width_height_original, anns)
+        bg_mask = self.anns_to_bg_mask(width_height_original, anns)
         bg_mask = bg_mask[::self.input_output_scale, ::self.input_output_scale]
 
         # valid area
@@ -81,3 +83,35 @@ class AnnRescaler(object):
             )
 
         return keypoint_sets, bg_mask, valid_area
+
+    def anns_to_keypoint_sets(self, anns):
+        """Ignore annotations of crowds."""
+        keypoint_sets = [ann['keypoints'] for ann in anns if not ann['iscrowd']]
+        if not keypoint_sets:
+            return np.zeros((0, self.n_keypoints, 3))
+
+        return np.stack(keypoint_sets)
+
+    @staticmethod
+    def anns_to_bg_mask(width_height, anns, include_annotated=True):
+        """Create background mask taking crowded annotations into account."""
+        mask = np.ones(width_height[::-1], dtype=np.bool)
+        for ann in anns:
+            if include_annotated and \
+            not ann['iscrowd'] and \
+            'keypoints' in ann and \
+            np.any(ann['keypoints'][:, 2] > 0):
+                continue
+
+            if 'mask' not in ann:
+                bb = ann['bbox'].copy()
+                bb[2:] += bb[:2]  # convert width and height to x2 and y2
+                bb[0] = np.clip(bb[0], 0, mask.shape[1] - 1)
+                bb[1] = np.clip(bb[1], 0, mask.shape[0] - 1)
+                bb[2] = np.clip(bb[2], 0, mask.shape[1] - 1)
+                bb[3] = np.clip(bb[3], 0, mask.shape[0] - 1)
+                bb = bb.astype(np.int)
+                mask[bb[1]:bb[3] + 1, bb[0]:bb[2] + 1] = 0
+                continue
+            mask[ann['mask']] = 0
+        return mask
--- openpifpaf/encoder/utils.py
+++ openpifpaf/encoder/utils.py
@@ -16,39 +16,6 @@ def create_sink(side):
     return sink
 
 
-def anns_to_keypoint_sets(anns):
-    """Ignore crowded annotations."""
-    keypoint_sets = [ann['keypoints'] for ann in anns if not ann['iscrowd']]
-    if not keypoint_sets:
-        return np.zeros((0, 17, 3))  # TODO: remove hard coded class number
-
-    return np.stack(keypoint_sets)
-
-
-def anns_to_bg_mask(width_height, anns, include_annotated=True):
-    """Create background mask taking crowded annotations into account."""
-    mask = np.ones(width_height[::-1], dtype=np.bool)
-    for ann in anns:
-        if include_annotated and \
-           not ann['iscrowd'] and \
-           'keypoints' in ann and \
-           np.any(ann['keypoints'][:, 2] > 0):
-            continue
-
-        if 'mask' not in ann:
-            bb = ann['bbox'].copy()
-            bb[2:] += bb[:2]  # convert width and height to x2 and y2
-            bb[0] = np.clip(bb[0], 0, mask.shape[1] - 1)
-            bb[1] = np.clip(bb[1], 0, mask.shape[0] - 1)
-            bb[2] = np.clip(bb[2], 0, mask.shape[1] - 1)
-            bb[3] = np.clip(bb[3], 0, mask.shape[0] - 1)
-            bb = bb.astype(np.int)
-            mask[bb[1]:bb[3] + 1, bb[0]:bb[2] + 1] = 0
-            continue
-        mask[ann['mask']] = 0
-    return mask
-
-
 def mask_valid_area(intensities, valid_area):
     if valid_area is None:
         return intensities
