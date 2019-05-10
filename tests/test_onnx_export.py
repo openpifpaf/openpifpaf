79e3b85063e03f5040429273812184774e51f911
Sven Kreiss <me@svenkreiss.com>
2019-05-10 09:15:15 +0200

    fix

--- tests/test_onnx_export.py
+++ tests/test_onnx_export.py
@@ -3,6 +3,7 @@ import os
 import openpifpaf.export_onnx
 
 
-def test_onnx_exportable():
-    openpifpaf.export_onnx.main()
-    assert os.path.exists('openpifpaf-resnet50.onnx')
+def test_onnx_exportable(tmp_path):
+    outfile = os.path.join(tmp_path, 'openpifpaf-resnet50.onnx')
+    openpifpaf.export_onnx.apply(checkpoint='resnet50', outfile=outfile)
+    assert os.path.exists(outfile)
