23a66b1ce490e4c7cd36330701da411af1bbedf0
Sven Kreiss <me@svenkreiss.com>
2019-05-10 10:54:01 +0200

    cli for ONNX export (#54)

--- tests/test_onnx_export.py
+++ tests/test_onnx_export.py
@@ -3,6 +3,9 @@ import os
 import openpifpaf.export_onnx
 
 
-def test_onnx_exportable():
-    openpifpaf.export_onnx.main()
-    assert os.path.exists('openpifpaf-resnet50.onnx')
+def test_onnx_exportable(tmpdir):
+    outfile = str(tmpdir.join('openpifpaf-resnet50.onnx'))
+    assert not os.path.exists(outfile)
+
+    openpifpaf.export_onnx.apply('resnet50', outfile, verbose=False)
+    assert os.path.exists(outfile)
