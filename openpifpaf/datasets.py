337b86da6230caa72e344ed6d352ea8a36140515
Sven Kreiss <me@svenkreiss.com>
2019-07-05 16:51:54 +0200

    update interface for PilImageList

--- openpifpaf/datasets.py
+++ openpifpaf/datasets.py
@@ -191,16 +191,21 @@ class ImageList(torch.utils.data.Dataset):
 
 
 class PilImageList(torch.utils.data.Dataset):
-    def __init__(self, images, image_transform=None):
+    def __init__(self, images, preprocess=None, image_transform=None):
         self.images = images
+        self.preprocess = preprocess
         self.image_transform = image_transform or transforms.image_transform
 
     def __getitem__(self, index):
         pil_image = self.images[index].copy().convert('RGB')
+
+        if self.preprocess is not None:
+            pil_image = self.preprocess(pil_image, [], None)[0]
+
         original_image = torchvision.transforms.functional.to_tensor(pil_image)
         image = self.image_transform(pil_image)
 
-        return index, original_image, image
+        return 'pilimage{}'.format(index), original_image, image
 
     def __len__(self):
         return len(self.images)
