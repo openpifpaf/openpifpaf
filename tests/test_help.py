29044c28a09f6bca6dc238fb172e98a03ddb99c8
Sven Kreiss <me@svenkreiss.com>
2019-08-14 10:58:57 -0500

    install opencv-python for tests

--- tests/test_help.py
+++ tests/test_help.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 import pytest
@@ -5,8 +6,11 @@ import pytest
 
 @pytest.mark.parametrize('module_name', ['predict', 'train', 'logs', 'webcam', 'eval_coco'])
 def test_predict(module_name):
-    with open('docs/cli-help-{}.txt'.format(module_name), 'w') as f:
+    out_file = 'docs/cli-help-{}.txt'.format(module_name)
+    with open(out_file, 'w') as f:
         subprocess.run([
             'python', '-m', 'openpifpaf.{}'.format(module_name),
             '--help',
         ], stdout=f)
+
+    assert os.path.getsize(out_file) > 0
