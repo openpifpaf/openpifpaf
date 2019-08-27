a6be7837ee3db682078c570eeaf68477b2a4c279
Sven Kreiss <me@svenkreiss.com>
2019-08-27 10:16:44 +0200

    PAF stack decoder

--- openpifpaf/decoder/__init__.py
+++ openpifpaf/decoder/__init__.py
@@ -3,6 +3,7 @@
 from .annotation import Annotation, AnnotationWithoutSkeleton
 from .decoder import Decoder
 from .factory import cli, factory_decode, factory_from_args
+from .paf_stack import PafStack
 from .pif import Pif
 from .pifpaf import PifPaf
 from .pifpaf2 import PifPaf2
