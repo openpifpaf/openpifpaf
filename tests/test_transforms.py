1bfcef57e16729b355d89d0bb832bfdd444e0918
Sven Kreiss <me@svenkreiss.com>
2019-08-30 15:42:48 +0200

    fix test

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -4,42 +4,6 @@ import PIL
 from openpifpaf import transforms
 
 
-def rescale_from_12_to_6(im_np, x, y):
-    print(im_np[0, :, 0])
-    im = PIL.Image.fromarray(im_np)
-
-    anns = [{
-        'keypoints': [[x, y, 2.0]],
-        'bbox': [0.0, 0.0, 12.0, 12.0],
-        'segmentation': None,
-    }]
-
-    transform = transforms.Compose([
-        transforms.NormalizeAnnotations(),
-        transforms.RescaleAbsolute(6),
-        transforms.EVAL_TRANSFORM,
-    ])
-
-    return transform(im, anns, None)
-
-
-def test_1():
-    im_in = np.zeros((12, 12, 3), dtype=np.uint8)
-    im_in[0, [5, 6]] = 255
-
-    im, anns, _ = rescale_from_12_to_6(im_in, 5.5, 0)
-    im = np.asarray(im)
-
-    print(anns)
-    assert anns[0]['bbox'].tolist() == [0.0, 0.0, 6.0, 6.0]
-    assert anns[0]['keypoints'][0, 0] == 2.5
-    assert anns[0]['keypoints'][0, 1] == -0.25
-
-
-    print(im[0, :, 0], im.shape)
-    assert im[0, :3, 0].tolist() == im[0, :2:-1, 0].tolist()  # symmetric
-
-
 def apply_transform(im_np, anns, transform=None):
     im = PIL.Image.fromarray(im_np)
 
@@ -73,6 +37,13 @@ def single_pixel_transform(x, y, transform, image_wh=(13, 11)):
         [image_yx[1], image_yx[0]],
         anns_transformed[0]['keypoints'][0][:2].tolist(),
     )
+
+
+def test_rescale_absolute(x=5, y=5):
+    image_xy, keypoint_xy = single_pixel_transform(
+        x, y, transforms.RescaleAbsolute(7), image_wh=(11, 11))
+    print(image_xy, keypoint_xy)
+    assert image_xy == keypoint_xy
 
 
 def test_crop(x=4, y=6):
