d9f7a174dd02e5787281295f3b123da95e91aac3
Sven Kreiss <me@svenkreiss.com>
2020-01-11 09:10:07 -0600

    gradual decay

--- openpifpaf/optimize.py
+++ openpifpaf/optimize.py
@@ -26,24 +26,28 @@ def cli(parser):
                          help='learning rate')
     group_s.add_argument('--lr-decay', default=[], nargs='+', type=float,
                          help='epochs at which to decay the learning rate')
+    group_s.add_argument('--lr-decay-factor', default=0.1, type=float,
+                         help='learning rate decay factor')
+    group_s.add_argument('--lr-decay-duration', default=1.0, type=float,
+                         help='learning rate decay duration in epochs')
     group_s.add_argument('--lr-burn-in-start-epoch', default=0, type=float,
                          help='starting epoch for burn-in')
     group_s.add_argument('--lr-burn-in-epochs', default=2, type=float,
                          help='number of epochs at the beginning with lower learning rate')
     group_s.add_argument('--lr-burn-in-factor', default=0.001, type=float,
                          help='learning pre-factor during burn-in')
-    group_s.add_argument('--lr-gamma', default=0.1, type=float,
-                         help='learning rate decay factor')
 
 
 class LearningRateLambda(object):
     def __init__(self, burn_in_duration, decay_schedule, *,
-                 gamma=0.1,
+                 decay_factor=0.1,
+                 decay_duration=1.0,
                  burn_in_start=0,
                  burn_in_factor=0.01):
         self.burn_in_duration = burn_in_duration
         self.decay_schedule = decay_schedule
-        self.gamma = gamma
+        self.decay_factor = decay_factor
+        self.decay_duration = decay_duration
         self.burn_in_start = burn_in_start
         self.burn_in_factor = burn_in_factor
 
@@ -59,8 +63,12 @@ class LearningRateLambda(object):
             )
 
         for d in self.decay_schedule:
-            if step_i >= d:
-                lambda_ *= self.gamma
+            if step_i >= d + self.decay_duration:
+                lambda_ *= self.decay_factor
+            elif step_i > d:
+                lambda_ *= self.decay_factor**(
+                    (step_i - d) / self.decay_duration
+                )
 
         return lambda_
 
@@ -90,7 +98,8 @@ def factory_lrscheduler(args, optimizer, training_batches_per_epoch):
         optimizer,
         [LearningRateLambda(args.lr_burn_in_epochs * training_batches_per_epoch,
                             [s * training_batches_per_epoch for s in args.lr_decay],
-                            gamma=args.lr_gamma,
+                            decay_factor=args.lr_decay_factor,
+                            decay_duration=args.lr_decay_duration * training_batches_per_epoch,
                             burn_in_start=args.lr_burn_in_start_epoch * training_batches_per_epoch,
                             burn_in_factor=args.lr_burn_in_factor)],
     )
