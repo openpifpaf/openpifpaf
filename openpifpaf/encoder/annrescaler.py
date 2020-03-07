caa5ae123b449d7c4adbd91983e15e45c9f48b58
Sven Kreiss <me@svenkreiss.com>
2020-03-07 10:01:40 +0100

    improved bg mask for small ignore regions

--- openpifpaf/encoder/annrescaler.py
+++ openpifpaf/encoder/annrescaler.py
@@ -12,7 +12,6 @@ class AnnRescaler(object):
 
         # background mask
         bg_mask = self.anns_to_bg_mask(width_height_original, anns)
-        bg_mask = bg_mask[::self.input_output_scale, ::self.input_output_scale]
 
         # valid area TODO use from meta
         valid_area = None
@@ -35,10 +34,12 @@ class AnnRescaler(object):
 
         return np.stack(keypoint_sets)
 
-    @staticmethod
-    def anns_to_bg_mask(width_height, anns, include_annotated=True):
+    def anns_to_bg_mask(self, width_height, anns, include_annotated=True):
         """Create background mask taking crowded annotations into account."""
-        mask = np.ones(width_height[::-1], dtype=np.bool)
+        mask = np.ones((
+            (width_height[1] - 1) // self.input_output_scale + 1,
+            (width_height[0] - 1) // self.input_output_scale + 1,
+        ), dtype=np.bool)
         for ann in anns:
             if include_annotated and \
                not ann['iscrowd'] and \
@@ -48,15 +49,16 @@ class AnnRescaler(object):
 
             if 'mask' not in ann:
                 bb = ann['bbox'].copy()
+                bb /= self.input_output_scale
                 bb[2:] += bb[:2]  # convert width and height to x2 and y2
-                bb[0] = np.clip(bb[0], 0, mask.shape[1] - 1)
-                bb[1] = np.clip(bb[1], 0, mask.shape[0] - 1)
-                bb[2] = np.clip(bb[2], 0, mask.shape[1] - 1)
-                bb[3] = np.clip(bb[3], 0, mask.shape[0] - 1)
-                bb = bb.astype(np.int)
-                mask[bb[1]:bb[3] + 1, bb[0]:bb[2] + 1] = 0
+                left = np.clip(int(bb[0]), 0, mask.shape[1] - 1)
+                top = np.clip(int(bb[1]), 0, mask.shape[0] - 1)
+                right = np.clip(int(np.ceil(bb[2])), left + 1, mask.shape[1])
+                bottom = np.clip(int(np.ceil(bb[3])), top + 1, mask.shape[0])
+                mask[top:bottom, left:right] = 0
                 continue
 
-            mask[ann['mask']] = 0
+            assert False  # because code below is not tested
+            mask[ann['mask'][::self.input_output_scale, ::self.input_output_scale]] = 0
 
         return mask
