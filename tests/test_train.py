50c0cfaba6ebe768cc2d51892ca49cc39d92f134
Sven Kreiss <me@svenkreiss.com>
2020-03-16 11:35:33 +0100

    fix

--- tests/test_train.py
+++ tests/test_train.py
@@ -28,7 +28,7 @@ PREDICT_COMMAND = [
 ]
 
 
-@pytest.importorskip('pycocotools')
+@pytest.mark.skipif(os.getenv('PIFPAFTRAINING') != '1', reason='env PIFPAFTRAINING is not set')
 def test_train(tmp_path):
     # train a model
     train_cmd = TRAIN_COMMAND + ['--out={}'.format(os.path.join(tmp_path, 'train_test.pkl'))]
