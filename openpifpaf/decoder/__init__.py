0e66572869f3fd77ec278ecfd7969c192fe0533e
Sven Kreiss <me@svenkreiss.com>
2020-03-27 12:33:19 +0100

    export FieldConfig

--- openpifpaf/decoder/__init__.py
+++ openpifpaf/decoder/__init__.py
@@ -2,6 +2,7 @@
 
 from .annotation import Annotation, AnnotationWithoutSkeleton
 from .factory import cli, configure, factory_decode, factory_from_args
+from .field_config import FieldConfig
 from .occupancy import Occupancy
 from .processor import Processor
 from .visualizer import Visualizer
