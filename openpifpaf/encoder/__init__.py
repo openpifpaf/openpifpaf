14a5e94fc89397fcab8c0e2fe852904ad9590c2c
Sven Kreiss <me@svenkreiss.com>
2020-03-31 15:12:01 +0200

    expose AnnRescaler

--- openpifpaf/encoder/__init__.py
+++ openpifpaf/encoder/__init__.py
@@ -1,5 +1,6 @@
 """Convert a set of keypoint coordinates into target fields."""
 
+from .annrescaler import AnnRescaler
 from .factory import cli, configure, factory, factory_head
 from .caf import Caf
 from .cif import Cif
