caa10f39e353fe60a92e0bed455a6b14a0ee4de0
Sven Kreiss <me@svenkreiss.com>
2020-04-12 13:13:11 +0200

    rename

--- openpifpaf/show/__init__.py
+++ openpifpaf/show/__init__.py
@@ -2,4 +2,4 @@ from .animation_frame import AnimationFrame
 from .canvas import canvas, image_canvas, load_image, white_screen
 from .cli import cli, configure
 from .fields import arrows, boxes, boxes_wh, circles, margins, quiver
-from .keypoint_painter import AnnotationPainter, CrowdPainter, DetectionPainter, KeypointPainter
+from .painters import AnnotationPainter, CrowdPainter, DetectionPainter, KeypointPainter
