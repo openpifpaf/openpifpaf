7d44afd79c2c452d62b615b549b3135186f633f9
Sven Kreiss <me@svenkreiss.com>
2020-04-12 22:15:57 +0200

    fix and simplify

--- openpifpaf/decoder/generator/cifdet.py
+++ openpifpaf/decoder/generator/cifdet.py
@@ -6,14 +6,11 @@ from ...annotation import AnnotationDet
 from ..field_config import FieldConfig
 from ..cif_hr import CifDetHr
 from ..cif_seeds import CifDetSeeds
-from ..occupancy import Occupancy
 
 LOG = logging.getLogger(__name__)
 
 
-class CifDet(object):
-    debug_visualizer = None
-
+class CifDet:
     def __init__(self, field_config: FieldConfig, categories):
         self.field_config = field_config
         self.categories = categories
@@ -30,24 +27,10 @@ class CifDet(object):
         cifhr = CifDetHr(self.field_config).fill(fields)
         seeds = CifDetSeeds(cifhr.accumulated, self.field_config).fill(fields)
 
-        occupied = Occupancy(cifhr.accumulated.shape, 2, min_scale=4)
-        annotations = []
-
-        def mark_occupied(ann):
-            width = min(ann.bbox[2], ann.bbox[3])
-            occupied.set(ann.field_i, ann.bbox[0], ann.bbox[1], width)
-
-        for v, f, x, y, w, h in seeds.get():
-            if occupied.get(f, x, y):
-                continue
-
-            ann = AnnotationDet(self.categories).set(f, v, (x - w/2.0, y - h/2.0, w, h))
-            annotations.append(ann)
-            mark_occupied(ann)
-
-        if self.debug_visualizer:
-            self.debug_visualizer.predicted(occupied)
+        annotations = [
+            AnnotationDet(self.categories).set(f, v, (x - w/2.0, y - h/2.0, w, h))
+            for v, f, x, y, w, h in seeds.get()
+        ]
 
         LOG.debug('annotations %d, %.3fs', len(annotations), time.perf_counter() - start)
-
         return annotations
