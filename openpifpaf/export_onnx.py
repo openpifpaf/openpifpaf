3e449a87e23a96723963854fc1a7e6d4c0b88c5f
Sven Kreiss <me@svenkreiss.com>
2020-04-25 16:58:06 +0200

    remove patch

--- openpifpaf/export_onnx.py
+++ openpifpaf/export_onnx.py
@@ -26,65 +26,6 @@ except ImportError:
     onnxsim = None
 
 
-# monkey patch
-class MonkeyPatches:
-    def __init__(self):
-        self.original_compositehead_patched_forward = \
-            openpifpaf.network.heads.CompositeField.forward
-
-    def apply(self):
-        openpifpaf.network.heads.CompositeField.forward = \
-            self.compositehead_patched_forward
-
-    def revert(self):
-        openpifpaf.network.heads.CompositeField.forward = \
-            self.original_compositehead_patched_forward
-
-    @staticmethod
-    def compositehead_patched_forward(self_, x):  # TODO: outdated, still necessary?
-        x = self_.dropout(x)
-
-        # classification
-        classes_x = [class_conv(x) for class_conv in self_.class_convs]
-        if not self_.training:
-            classes_x = [torch.sigmoid(class_x) for class_x in classes_x]
-
-        # regressions
-        regs_x = [reg_conv(x) * self_.dilation for reg_conv in self_.reg_convs]
-        regs_x_spread = [reg_spread(x) for reg_spread in self_.reg_spreads]
-        # regs_x_spread = [torch.nn.functional.leaky_relu(x + 3.0) - 3.0 for x in regs_x_spread]
-        # problem for ONNX is the "- 3.0"
-        regs_x_spread = [torch.clamp(x, min=-3.0) for x in regs_x_spread]
-
-        # scale
-        scales_x = [scale_conv(x) for scale_conv in self_.scale_convs]
-        scales_x = [torch.nn.functional.relu(scale_x) for scale_x in scales_x]
-
-        for _ in range(self_._quad):  # pylint: disable=protected-access
-            classes_x = [self_.dequad_op(class_x)[:, :, :-1, :-1]
-                         for class_x in classes_x]
-            regs_x = [self_.dequad_op(reg_x)[:, :, :-1, :-1]
-                      for reg_x in regs_x]
-            regs_x_spread = [self_.dequad_op(reg_x_spread)[:, :, :-1, :-1]
-                             for reg_x_spread in regs_x_spread]
-            scales_x = [self_.dequad_op(scale_x)[:, :, :-1, :-1]
-                        for scale_x in scales_x]
-
-        # reshape regressions:
-        # Accessing the shape of .data instead of the shape of reg_x saves
-        # nodes in the ONNX graph.
-        regs_x = [
-            reg_x.reshape(reg_x.data.shape[0],
-                          reg_x.data.shape[1] // 2,
-                          2,
-                          reg_x.data.shape[2],
-                          reg_x.data.shape[3])
-            for reg_x in regs_x
-        ]
-
-        return classes_x + regs_x + regs_x_spread + scales_x
-
-
 class GetPif(torch.nn.Module):
     def forward(self, heads):  # pylint: disable=arguments-differ
         return heads[0]
@@ -96,9 +37,6 @@ class GetPifC(torch.nn.Module):
 
 
 def apply(model, outfile, verbose=True):
-    # monkey_patches = MonkeyPatches()
-    # monkey_patches.apply()
-
     # dummy_input = torch.randn(1, 3, 193, 257)
     dummy_input = torch.randn(1, 3, 97, 129)
     # model = torch.nn.Sequential(model, GetPifC())
@@ -145,8 +83,6 @@ def apply(model, outfile, verbose=True):
         #     'paf_b2': {0: 'batch', 2: 'fheight', 3: 'fwidth'},
         # },
     )
-
-    # monkey_patches.revert()
 
 
 def optimize(infile, outfile=None):
