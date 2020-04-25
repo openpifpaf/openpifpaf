51bac3f510eebab7ce9c6b4fa772a906cea4cb07
Sven Kreiss <me@svenkreiss.com>
2020-04-25 19:54:17 +0200

    expose classes

--- openpifpaf/decoder/__init__.py
+++ openpifpaf/decoder/__init__.py
@@ -1,5 +1,8 @@
 """Collections of decoders: fields to annotations."""
 
+from .caf_scored import CafScored
+from .cif_hr import CifHr, CifDetHr
+from .cif_seeds import CifSeeds
 from .factory import cli, configure, factory_decode, factory_from_args
 from .field_config import FieldConfig
 from .occupancy import Occupancy
