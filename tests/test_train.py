c376451f9dee4dc7c36c3113e9e9c3a22b5c59d5
Sven Kreiss <me@svenkreiss.com>
2020-04-25 20:18:17 +0200

    fix

--- tests/test_train.py
+++ tests/test_train.py
@@ -6,14 +6,13 @@ import pytest
 TRAIN_COMMAND = [
     'python3', '-m', 'openpifpaf.train',
     '--lr=1e-3',
-    '--momentum=0.95',
+    '--momentum=0.9',
     '--epochs=1',
     '--batch-size=1',
     '--basenet=resnet18', '--no-pretrain',
     '--head-quad=1',
-    '--headnets', 'pif', 'paf', 'paf25',
+    '--headnets', 'cif', 'caf', 'caf25',
     '--square-edge=161',
-    '--lambdas', '10', '1', '1', '15', '1', '1', '15', '1', '1',
     '--train-annotations', 'tests/coco/train1.json',
     '--train-image-dir', 'tests/coco/images/',
     '--val-annotations', 'tests/coco/train1.json',
@@ -23,7 +22,6 @@ TRAIN_COMMAND = [
 
 PREDICT_COMMAND = [
     'python3', '-m', 'openpifpaf.predict',
-    '--output-types=json',
     'tests/coco/images/puppy_dog.jpg',
 ]
 
@@ -42,10 +40,10 @@ def test_train(tmp_path):
     # run a prediction with that model
     predict_cmd = PREDICT_COMMAND + [
         '--checkpoint={}'.format(os.path.join(tmp_path, final_model)),
-        '--output-directory={}'.format(tmp_path),
+        '--json-output={}'.format(tmp_path),
     ]
     print(' '.join(predict_cmd))
     subprocess.run(predict_cmd, check=True, capture_output=True)
     print(os.listdir(tmp_path))
 
-    assert 'puppy_dog.jpg.pifpaf.json' in os.listdir(tmp_path)
+    assert 'puppy_dog.jpg.predictions.json' in os.listdir(tmp_path)
