68a00aaf997a2b39dbe32303c7bd3d7204fc1685
Sven Kreiss <me@svenkreiss.com>
2020-06-15 12:54:39 +0200

    separate logo generation from book

---
+++ guide/artwork.py
@@ -0,0 +1,37 @@
+import openpifpaf
+from openpifpaf.datasets.constants import (
+    COCO_KEYPOINTS,
+    COCO_PERSON_SKELETON,
+    COCO_UPRIGHT_POSE,
+)
+
+
+def main():
+    ann = openpifpaf.Annotation(keypoints=COCO_KEYPOINTS, skeleton=COCO_PERSON_SKELETON)
+    ann.set(COCO_UPRIGHT_POSE)
+
+    # favicon
+    keypoint_painter = openpifpaf.show.KeypointPainter(
+        color_connections=True, linewidth=48, markersize=0)
+    openpifpaf.datasets.constants.draw_ann(
+        ann,
+        keypoint_painter=keypoint_painter,
+        aspect='equal',
+        margin=0.8,
+        frameon=False,
+        filename='favicon.png',
+    )
+
+    # logo
+    keypoint_painter = openpifpaf.show.KeypointPainter(
+        color_connections=True, linewidth=12)
+    openpifpaf.datasets.constants.draw_ann(
+        ann,
+        keypoint_painter=keypoint_painter,
+        frameon=False,
+        filename='logo.png',
+    )
+
+
+if __name__ == '__main__':
+    main()
