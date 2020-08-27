4d5b87a0e39727acba3b8664593029b5e8338007
Sven Kreiss <me@svenkreiss.com>
2020-08-27 17:55:15 +0200

    add output tensor descriptions

--- openpifpaf/export_coreml.py
+++ openpifpaf/export_coreml.py
@@ -28,11 +28,15 @@ def apply(model, outfile, input_w=129, input_h=97):
         # classifier_config = ct.ClassifierConfig(class_labels)
         minimum_deployment_target=coremltools.target.iOS13,
     )
+    # coremltools.models.utils.rename_feature(
+    #     coreml_model._spec, coreml_model._spec.description.output[0].name, 'cif_head')
+    # coremltools.models.utils.rename_feature(
+    #     coreml_model._spec, coreml_model._spec.description.output[1].name, 'caf_head')
 
     # Meta
     coreml_model.input_description['image'] = 'Input image to be classified'
-    # coreml_model.output_description['cif'] = 'Composite Intensity Field'
-    # coreml_model.output_description['caf'] = 'Composite Association Field'
+    coreml_model.output_description['cif_head'] = 'Composite Intensity Field'
+    coreml_model.output_description['caf_head'] = 'Composite Association Field'
     coreml_model.author = 'Kreiss, Bertoni, Alahi: Composite Fields for Human Pose Estimation'
     coreml_model.license = 'Please see https://github.com/vita-epfl/openpifpaf'
     coreml_model.short_description = 'Composite Fields for Human Pose Estimation'
