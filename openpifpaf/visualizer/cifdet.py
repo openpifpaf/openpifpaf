514ed7acf5f4be60bbe90f81a5d55714f9dce434
Sven Kreiss <me@svenkreiss.com>
2020-08-28 19:10:46 +0200

    image canvas margin (#274)
    Some color bars were clipped. This PR adds the margin keyword to image_canvas() and uses it in the visualizers with color bars.

--- openpifpaf/visualizer/cifdet.py
+++ openpifpaf/visualizer/cifdet.py
@@ -53,7 +53,7 @@ class CifDet(BaseVisualizer):
         for f in self.indices:
             LOG.debug('%s', self.categories[f])
 
-            with self.image_canvas(self._processed_image) as ax:
+            with self.image_canvas(self._processed_image, margin=[0.0, 0.01, 0.05, 0.01]) as ax:
                 im = ax.imshow(self.scale_scalar(confidences[f], self.stride),
                                alpha=0.9, vmin=0.0, vmax=1.0, cmap=CMAP_GREENS_NAN)
                 self.colorbar(ax, im)
@@ -67,7 +67,7 @@ class CifDet(BaseVisualizer):
             LOG.debug('%s', self.categories[f])
             confidence_field = confidence_fields[f] if confidence_fields is not None else None
 
-            with self.image_canvas(self._processed_image) as ax:
+            with self.image_canvas(self._processed_image, margin=[0.0, 0.01, 0.05, 0.01]) as ax:
                 show.white_screen(ax, alpha=0.5)
                 if annotations:
                     self.detection_painter.annotations(ax, annotations, color='gray')
