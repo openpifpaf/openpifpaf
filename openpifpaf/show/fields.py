0c7482ade93df94b31059d7475e918514e93d601
Sven Kreiss <me@svenkreiss.com>
2020-09-04 07:05:36 +0200

    remove matplotlib warning

--- openpifpaf/show/fields.py
+++ openpifpaf/show/fields.py
@@ -49,7 +49,7 @@ def quiver(ax, vector_field, *,
         ax.add_artist(circle)
 
     return ax.quiver(x[s], y[s], u[s] - x[s], v[s] - y[s], c[s],
-                     angles='xy', scale_units='xy', scale=1, zOrder=10, **kwargs)
+                     angles='xy', scale_units='xy', scale=1, zorder=10, **kwargs)
 
 
 def margins(ax, vector_field, *,
@@ -99,7 +99,7 @@ def arrows(ax, fourd, xy_scale=1.0, threshold=0.0, **kwargs):
     c = np.min(fourd[:, 2], axis=0)
     s = np.argsort(c)
     return ax.quiver(x1[s], y1[s], (x2 - x1)[s], (y2 - y1)[s], c[s],
-                     angles='xy', scale_units='xy', scale=1, zOrder=10, **kwargs)
+                     angles='xy', scale_units='xy', scale=1, zorder=10, **kwargs)
 
 
 def boxes(ax, sigma_field, **kwargs):
