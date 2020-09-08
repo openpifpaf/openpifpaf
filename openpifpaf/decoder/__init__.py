c2197d8e0407ffb2bfea3c2e6b6389c53bf5c079
Sven Kreiss <me@svenkreiss.com>
2020-09-08 16:19:12 +0200

    expose CifDetSeeds

--- openpifpaf/decoder/__init__.py
+++ openpifpaf/decoder/__init__.py
@@ -2,7 +2,7 @@
 
 from .caf_scored import CafScored
 from .cif_hr import CifHr, CifDetHr
-from .cif_seeds import CifSeeds
+from .cif_seeds import CifSeeds, CifDetSeeds
 from .factory import cli, configure, factory
 from .generator.cifcaf import CifCaf
 from .generator.cifdet import CifDet
