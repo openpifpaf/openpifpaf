d05ca10a3e87854fbbdd1b641026174851d3bf6c
george-adaimi <georgeadaimi@gmail.com>
2020-10-23 19:18:00 +0200

    Painter plugin feature (#291)

--- openpifpaf/show/__init__.py
+++ openpifpaf/show/__init__.py
@@ -4,4 +4,5 @@ from .animation_frame import AnimationFrame
 from .canvas import Canvas, canvas, image_canvas, white_screen
 from .cli import cli, configure
 from .fields import arrows, boxes, boxes_wh, circles, margins, quiver
-from .painters import AnnotationPainter, CrowdPainter, DetectionPainter, KeypointPainter
+from .painters import CrowdPainter, DetectionPainter, KeypointPainter
+from .annotation_painter import AnnotationPainter
--- openpifpaf/show/painters.py
+++ openpifpaf/show/painters.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 import logging
 
 import numpy as np
@@ -13,35 +12,6 @@ except ImportError:
 
 
 LOG = logging.getLogger(__name__)
-
-
-class AnnotationPainter:
-    def __init__(self, *,
-                 xy_scale=1.0,
-                 keypoint_painter=None,
-                 crowd_painter=None,
-                 detection_painter=None):
-        self.painters = {
-            'Annotation': keypoint_painter or KeypointPainter(xy_scale=xy_scale),
-            'AnnotationCrowd': crowd_painter or CrowdPainter(xy_scale=xy_scale),
-            'AnnotationDet': detection_painter or DetectionPainter(xy_scale=xy_scale),
-        }
-
-    def annotations(self, ax, annotations, *,
-                    color=None, colors=None, texts=None, subtexts=None):
-        by_classname = defaultdict(list)
-        for ann_i, ann in enumerate(annotations):
-            by_classname[ann.__class__.__name__].append((ann_i, ann))
-
-        for classname, i_anns in by_classname.items():
-            anns = [ann for _, ann in i_anns]
-            this_colors = [colors[i] for i, _ in i_anns] if colors else None
-            this_texts = [texts[i] for i, _ in i_anns] if texts else None
-            this_subtexts = [subtexts[i] for i, _ in i_anns] if subtexts else None
-            self.painters[classname].annotations(
-                ax, anns,
-                color=color, colors=this_colors, texts=this_texts, subtexts=this_subtexts)
-
 
 class DetectionPainter:
     def __init__(self, *, xy_scale=1.0):
