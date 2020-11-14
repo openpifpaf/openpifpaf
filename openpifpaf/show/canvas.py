cbf74e82ca6b494f9dde71c3ec0cae913942f237
Sven Kreiss <me@svenkreiss.com>
2020-11-14 11:44:46 +0100

    lint

--- openpifpaf/show/canvas.py
+++ openpifpaf/show/canvas.py
@@ -5,9 +5,9 @@ import os
 import numpy as np
 
 try:
-    import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt  # pylint: disable=import-error
 except ModuleNotFoundError as err:
-    if err.name not in ('matplotlib', 'matplotlib.pyplot'):
+    if err.name != 'matplotlib':
         raise err
     plt = None
 
