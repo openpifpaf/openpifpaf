95f8dc0ea0b08dbb99271a8cbdfb1c8a5a71c10e
Sven Kreiss <me@svenkreiss.com>
2020-11-24 16:11:13 +0100

    bbox in gt annotation (#317)

--- openpifpaf/transforms/toannotations.py
+++ openpifpaf/transforms/toannotations.py
@@ -30,7 +30,12 @@ class ToKpAnnotations:
                 self.skeleton_by_category[ann['category_id']],
                 categories=self.categories,
             )
-            .set(ann['keypoints'], category_id=ann['category_id'], fixed_score=None)
+            .set(
+                ann['keypoints'],
+                category_id=ann['category_id'],
+                fixed_score='',
+                fixed_bbox=ann.get('bbox'),
+            )
             for ann in anns
             if not ann['iscrowd'] and np.any(ann['keypoints'][2::3] > 0.0)
         ]
@@ -43,7 +48,11 @@ class ToDetAnnotations:
     def __call__(self, anns):
         return [
             AnnotationDet(categories=self.categories)
-            .set(ann['category_id'], None, ann['bbox'])
+            .set(
+                ann['category_id'],
+                None,
+                ann['bbox'],
+            )
             for ann in anns
             if not ann['iscrowd'] and np.any(ann['bbox'])
         ]
@@ -56,7 +65,10 @@ class ToCrowdAnnotations:
     def __call__(self, anns):
         return [
             AnnotationCrowd(categories=self.categories)
-            .set(ann.get('category_id', 1), ann['bbox'])
+            .set(
+                ann.get('category_id', 1),
+                ann['bbox'],
+            )
             for ann in anns
             if ann['iscrowd']
         ]
