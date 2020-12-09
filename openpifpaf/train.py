c9e3fe729d17b71de4f1ae09595a7aa23e9e55e8
Sven Kreiss <me@svenkreiss.com>
2020-12-09 15:15:47 +0100

    configurable trainer (#322)

--- openpifpaf/train.py
+++ openpifpaf/train.py
@@ -52,6 +52,7 @@ def cli():
     logger.cli(parser)
     network.cli(parser)
     network.losses.cli(parser)
+    network.Trainer.cli(parser)
     encoder.cli(parser)
     optimize.cli(parser)
     datasets.cli(parser)
@@ -60,31 +61,8 @@ def cli():
 
     parser.add_argument('-o', '--output', default=None,
                         help='output file')
-    parser.add_argument('--stride-apply', default=1, type=int,
-                        help='apply and reset gradients every n batches')
-    parser.add_argument('--epochs', default=75, type=int,
-                        help='number of epochs to train')
-    parser.add_argument('--val-interval', default=1, type=int,
-                        help='validation run every n epochs')
-    parser.add_argument('--train-batches', default=None, type=int,
-                        help='number of train batches')
-    parser.add_argument('--val-batches', default=None, type=int,
-                        help='number of val batches')
-    parser.add_argument('--fix-batch-norm',
-                        default=False, const=True, type=int, nargs='?',
-                        help='fix batch norm running statistics (optionally specify epoch)')
-    parser.add_argument('--ema', default=1e-2, type=float,
-                        help='ema decay constant')
-    parser.add_argument('--clip-grad-norm', default=0.0, type=float,
-                        help='clip grad norm: specify largest change for single param')
-    parser.add_argument('--log-interval', default=11, type=int,
-                        help='log loss every n steps')
     parser.add_argument('--disable-cuda', action='store_true',
                         help='disable CUDA')
-
-    group = parser.add_argument_group('debug')
-    group.add_argument('--profile', default=None,
-                       help='enables profiling. specify path for chrome tracing file')
 
     args = parser.parse_args()
 
@@ -110,6 +88,7 @@ def cli():
 
     network.configure(args)
     network.losses.configure(args)
+    network.Trainer.configure(args)
     encoder.configure(args)
     datasets.configure(args)
     show.configure(args)
@@ -140,23 +119,14 @@ def main():
         net, loss, optimizer, args.output,
         lr_scheduler=lr_scheduler,
         device=args.device,
-        fix_batch_norm=args.fix_batch_norm,
-        stride_apply=args.stride_apply,
-        ema_decay=args.ema,
-        log_interval=args.log_interval,
-        train_profile=args.profile,
         model_meta_data={
             'args': vars(args),
             'version': __version__,
             'plugin_versions': plugin.versions(),
             'hostname': socket.gethostname(),
         },
-        clip_grad_norm=args.clip_grad_norm,
-        val_interval=args.val_interval,
-        n_train_batches=args.train_batches,
-        n_val_batches=args.val_batches,
     )
-    trainer.loop(train_loader, val_loader, args.epochs, start_epoch=start_epoch)
+    trainer.loop(train_loader, val_loader, start_epoch=start_epoch)
 
 
 if __name__ == '__main__':
