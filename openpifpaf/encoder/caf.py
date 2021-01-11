5377c7e2923d6902b3753e2fb65760fe338b2ddc
Sven Kreiss <me@svenkreiss.com>
2021-01-11 14:08:09 +0100

    CAF: support fill_values

--- openpifpaf/encoder/caf.py
+++ openpifpaf/encoder/caf.py
@@ -8,7 +8,7 @@ import torch
 from .annrescaler import AnnRescaler
 from .. import headmeta
 from ..visualizer import Caf as CafVisualizer
-from ..utils import create_sink, mask_valid_area
+from ..utils import mask_valid_area
 
 LOG = logging.getLogger(__name__)
 
@@ -46,7 +46,7 @@ class CafGenerator:
         if self.config.fixed_size:
             assert self.config.aspect_ratio == 0.0
 
-        LOG.debug('only_in_field_of_view = %s, paf min size = %d',
+        LOG.debug('only_in_field_of_view = %s, caf min size = %d',
                   config.meta.only_in_field_of_view,
                   self.config.min_size)
 
@@ -63,13 +63,14 @@ class CafGenerator:
         width_height_original = image.shape[2:0:-1]
 
         keypoint_sets = self.rescaler.keypoint_sets(anns)
+        fill_values = self.rescaler.fill_values(anns)
         bg_mask = self.rescaler.bg_mask(anns, width_height_original,
                                         crowd_margin=(self.config.min_size - 1) / 2)
         valid_area = self.rescaler.valid_area(meta)
         LOG.debug('valid area: %s', valid_area)
 
         self.init_fields(bg_mask)
-        self.fill(keypoint_sets)
+        self.fill(keypoint_sets, fill_values)
         fields = self.fields(valid_area)
 
         self.visualizer.processed_image(image)
@@ -95,9 +96,9 @@ class CafGenerator:
         self.fields_reg_l[:, p:-p, p:-p][:, bg_mask == 0] = 1.0
         self.intensities[:, p:-p, p:-p][:, bg_mask == 0] = np.nan
 
-    def fill(self, keypoint_sets):
-        for keypoints in keypoint_sets:
-            self.fill_keypoints(keypoints)
+    def fill(self, keypoint_sets, fill_values):
+        for keypoints, fill_value in zip(keypoint_sets, fill_values):
+            self.fill_keypoints(keypoints, fill_value)
 
     def shortest_sparse(self, joint_i, keypoints):
         shortest = np.inf
@@ -115,7 +116,7 @@ class CafGenerator:
 
         return shortest
 
-    def fill_keypoints(self, keypoints):
+    def fill_keypoints(self, keypoints, fill_values):
         scale = self.rescaler.scale(keypoints)
         for paf_i, (joint1i, joint2i) in enumerate(self.skeleton_m1):
             joint1 = keypoints[joint1i]
@@ -152,81 +153,88 @@ class CafGenerator:
                 if out_field_of_view_1 or out_field_of_view_2:
                     continue
 
-            if self.config.meta.sigmas is None:
-                scale1, scale2 = scale, scale
-            else:
-                scale1 = scale * self.config.meta.sigmas[joint1i]
-                scale2 = scale * self.config.meta.sigmas[joint2i]
+            self.fill_association(paf_i, joint1, joint2, scale, fill_values)
 
-            self.fill_association(paf_i, joint1, joint2, scale1, scale2)
-
-    def fill_association(self, paf_i, joint1, joint2, scale1, scale2):
+    def fill_association(self, paf_i, joint1, joint2, scale, fill_values):
         # offset between joints
         offset = joint2[:2] - joint1[:2]
         offset_d = np.linalg.norm(offset)
 
         # dynamically create s
         s = max(self.config.min_size, int(offset_d * self.config.aspect_ratio))
-        # s = max(s, min(int(scale1), int(scale2)))
-        sink = create_sink(s)
-        s_offset = (s - 1.0) / 2.0
+
+        # meshgrid: coordinate matrix
+        xyv = np.stack(np.meshgrid(
+            np.linspace(-0.5 * (s - 1), 0.5 * (s - 1), s),
+            np.linspace(-0.5 * (s - 1), 0.5 * (s - 1), s),
+        ), axis=-1).reshape(-1, 2)
 
         # set fields
         num = max(2, int(np.ceil(offset_d)))
-        fmargin = (s_offset + 1) / (offset_d + np.spacing(1))
+        fmargin = (s / 2) / (offset_d + np.spacing(1))
         fmargin = np.clip(fmargin, 0.25, 0.4)
         # fmargin = 0.0
         frange = np.linspace(fmargin, 1.0 - fmargin, num=num)
         if self.config.fixed_size:
             frange = [0.5]
+        filled_ij = set()
         for f in frange:
-            fij = np.round(joint1[:2] + f * offset - s_offset) + self.config.padding
-            fminx, fminy = int(fij[0]), int(fij[1])
-            fmaxx, fmaxy = fminx + s, fminy + s
-            if fminx < 0 or fmaxx > self.intensities.shape[2] or \
-               fminy < 0 or fmaxy > self.intensities.shape[1]:
-                continue
-            fxy = fij - self.config.padding + s_offset
+            for xyo in xyv:
+                fij = np.round(joint1[:2] + f * offset + xyo).astype(np.int) + self.config.padding
+                if fij[0] < 0 or fij[0] >= self.intensities.shape[2] or \
+                   fij[1] < 0 or fij[1] >= self.intensities.shape[1]:
+                    continue
 
-            # precise floating point offset of sinks
-            joint1_offset = (joint1[:2] - fxy).reshape(2, 1, 1)
-            joint2_offset = (joint2[:2] - fxy).reshape(2, 1, 1)
-            sink1 = sink + joint1_offset
-            sink2 = sink + joint2_offset
+                # convert to hashable coordinate and check whether
+                # it was processed before
+                fij_int = (int(fij[0]), int(fij[1]))
+                if fij_int in filled_ij:
+                    continue
+                filled_ij.add(fij_int)
 
-            # mask
-            # perpendicular distance computation:
-            # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
-            # Coordinate systems for this computation is such that
-            # joint1 is at (0, 0).
-            sink_l = np.fabs(
-                offset[1] * sink1[0]
-                - offset[0] * sink1[1]
-            ) / (offset_d + 0.01)
-            mask = sink_l < self.fields_reg_l[paf_i, fminy:fmaxy, fminx:fmaxx]
-            mask_peak = np.logical_and(mask, np.linalg.norm(sink1 + sink2, axis=0) < 0.7)
-            self.fields_reg_l[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = sink_l[mask]
+                fxy = fij - self.config.padding
 
-            # update intensity
-            self.intensities[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = 1.0
-            self.intensities[paf_i, fminy:fmaxy, fminx:fmaxx][mask_peak] = 1.0
+                # mask
+                # perpendicular distance computation:
+                # https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+                # Coordinate systems for this computation is such that
+                # joint1 is at (0, 0).
+                f_offset = fxy - joint1[:2]
+                sink_l = np.fabs(
+                    offset[1] * f_offset[0]
+                    - offset[0] * f_offset[1]
+                ) / (offset_d + 0.01)
+                if sink_l > self.fields_reg_l[paf_i, fij[1], fij[0]]:
+                    continue
+                self.fields_reg_l[paf_i, fij[1], fij[0]] = sink_l
 
-            # update regressions
-            patch1 = self.fields_reg1[paf_i, :, fminy:fmaxy, fminx:fmaxx]
-            patch1[:, mask] = sink1[:, mask]
-            patch2 = self.fields_reg2[paf_i, :, fminy:fmaxy, fminx:fmaxx]
-            patch2[:, mask] = sink2[:, mask]
+                self.fill_field_values(paf_i, fij, fxy, scale, fill_values)
 
-            # update bmin
-            bmin = self.config.bmin / self.config.meta.stride
-            self.fields_bmin1[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = bmin
-            self.fields_bmin2[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = bmin
+    def fill_field_values(self, paf_i, fij, fxy, scale, fill_values):
+        joint1i, joint2i = self.skeleton_m1[paf_i]
 
-            # update scale
-            assert np.isnan(scale1) or 0.0 < scale1 < 100.0
-            self.fields_scale1[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = scale1
-            assert np.isnan(scale2) or 0.0 < scale2 < 100.0
-            self.fields_scale2[paf_i, fminy:fmaxy, fminx:fmaxx][mask] = scale2
+        # update intensity
+        self.intensities[paf_i, fij[1], fij[0]] = 1.0
+
+        # update regressions
+        self.fields_reg1[paf_i, :, fij[1], fij[0]] = fill_values[joint1i][:2] - fxy
+        self.fields_reg2[paf_i, :, fij[1], fij[0]] = fill_values[joint2i][:2] - fxy
+
+        # update bmin
+        bmin = self.config.bmin / self.config.meta.stride
+        self.fields_bmin1[paf_i, fij[1], fij[0]] = bmin
+        self.fields_bmin2[paf_i, fij[1], fij[0]] = bmin
+
+        # update scale
+        if self.config.meta.sigmas is None:
+            scale1, scale2 = scale, scale
+        else:
+            scale1 = scale * self.config.meta.sigmas[joint1i]
+            scale2 = scale * self.config.meta.sigmas[joint2i]
+        assert np.isnan(scale1) or 0.0 < scale1 < 100.0
+        self.fields_scale1[paf_i, fij[1], fij[0]] = scale1
+        assert np.isnan(scale2) or 0.0 < scale2 < 100.0
+        self.fields_scale2[paf_i, fij[1], fij[0]] = scale2
 
     def fields(self, valid_area):
         p = self.config.padding
