c33ca49d6fb132c6ec2f657364830af2f4919c40
Sven Kreiss <me@svenkreiss.com>
2021-02-08 19:29:56 +0100

    Add coordinate_digits=2 arg to json_data(). Generalize rescale() to separate x and y scale factors.

--- openpifpaf/annotation.py
+++ openpifpaf/annotation.py
@@ -66,12 +66,22 @@ class Annotation(Base):
         return self
 
     def rescale(self, scale_factor):
-        self.data[:, 0:2] *= scale_factor
+        if len(scale_factor) == 2:
+            scale_x, scale_y = scale_factor
+            scale_factor = 0.5 * (scale_x + scale_y)
+        else:
+            scale_x = scale_factor
+            scale_y = scale_factor
+
+        self.data[:, 0] *= scale_x
+        self.data[:, 1] *= scale_y
         if self.joint_scales is not None:
             self.joint_scales *= scale_factor
         for _, __, c1, c2 in self.decoding_order:
-            c1[:2] *= scale_factor
-            c2[:2] *= scale_factor
+            c1[0:1] *= scale_x
+            c1[1:2] *= scale_y
+            c2[0:1] *= scale_x
+            c2[1:2] *= scale_y
         return self
 
     def fill_joint_scales(self, scales, hr_scale=1.0):
@@ -105,20 +115,20 @@ class Annotation(Base):
             np.max(self.data[m, 1]) - np.min(self.data[m, 1]),
         )
 
-    def json_data(self):
+    def json_data(self, coordinate_digits=2):
         """Data ready for json dump."""
 
         # avoid visible keypoints becoming invisible due to rounding
         v_mask = self.data[:, 2] > 0.0
         keypoints = np.copy(self.data)
         keypoints[v_mask, 2] = np.maximum(0.01, keypoints[v_mask, 2])
-        keypoints = np.around(keypoints.astype(np.float64), 2)
+        keypoints = np.around(keypoints.astype(np.float64), coordinate_digits)
 
         # convert to float64 before rounding because otherwise extra digits
         # will be added when converting to Python type
         data = {
             'keypoints': keypoints.reshape(-1).tolist(),
-            'bbox': [round(float(c), 2) for c in self.bbox()],
+            'bbox': [round(float(c), coordinate_digits) for c in self.bbox()],
             'score': max(0.001, round(self.score, 3)),
             'category_id': self.category_id,
         }
