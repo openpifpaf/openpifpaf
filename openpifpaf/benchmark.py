37ca1f2bf5303b55cb5152ae68893a2591daa460
Sven Kreiss <me@svenkreiss.com>
2021-02-23 12:11:46 +0100

    fix dense benchmark

--- openpifpaf/benchmark.py
+++ openpifpaf/benchmark.py
@@ -233,9 +233,12 @@ def main():
             Ablation('.greedy', eval_args + ['--greedy']),
             Ablation('.no-reverse', eval_args + ['--no-reverse-match']),
             Ablation('.greedy.no-reverse', eval_args + ['--greedy', '--no-reverse-match']),
-            Ablation('.greedy.dense', eval_args + ['--greedy', '--dense-connections']),
-            Ablation('.dense', eval_args + ['--dense-connections']),
-            Ablation('.dense.hierarchy', eval_args + ['--dense-connections=0.1']),
+            Ablation('.greedy.dense', eval_args + [
+                '--greedy', '--cocokp-with-dense', '--dense-connections']),
+            Ablation('.dense', eval_args + [
+                '--cocokp-with-dense', '--dense-connections']),
+            Ablation('.dense.hierarchy', eval_args + [
+                '--cocokp-with-dense', '--dense-connections=0.1']),
         ]
     if args.v012_ablation_2:
         eval_args_nofc = [a for a in eval_args
