8cfa6848b92d10681a7b15ca506b6e8b2b3d78a5
Sven Kreiss <me@svenkreiss.com>
2021-02-23 07:48:03 +0100

    new pretrained models: shufflenetv2k16-withdense and shufflenetv2k30

--- openpifpaf/network/factory.py
+++ openpifpaf/network/factory.py
@@ -28,8 +28,12 @@ CHECKPOINT_URLS = {
     'shufflenetv2x2': PRETRAINED_UNAVAILABLE,
     'shufflenetv2k16': ('http://github.com/vita-epfl/openpifpaf-torchhub/releases/download/'
                         'v0.12b4/shufflenetv2k16-210214-123448-cocokp-o10s-e2ae3708.pkl'),
+    'shufflenetv2k16-withdense': (
+        'http://github.com/vita-epfl/openpifpaf-torchhub/releases/download/'
+        'v0.12b4/shufflenetv2k16-210221-131426-cocokp-o10s-627d901e.pkl'
+    ),
     'shufflenetv2k30': ('http://github.com/vita-epfl/openpifpaf-torchhub/releases/download/'
-                        'v0.12a7/shufflenetv2k30-201123-174748-cocokp-o10s-bc89e3d5.pkl'),
+                        'v0.12b4/shufflenetv2k30-210217-075056-cocokp-o10s-6f9daa84.pkl'),
     'shufflenetv2k44': PRETRAINED_UNAVAILABLE,
 }
 
