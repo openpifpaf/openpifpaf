5d1268439a97402117d2611db47919032e4b3167
Sven Kreiss <me@svenkreiss.com>
2021-02-24 20:42:18 +0100

    mark slow

--- tests/test_train.py
+++ tests/test_train.py
@@ -26,7 +26,7 @@ PREDICT_COMMAND = [
 ]
 
 
-@pytest.mark.train
+@pytest.mark.slow
 def test_train(tmp_path):
     # train a model
     train_cmd = TRAIN_COMMAND + ['--out={}'.format(os.path.join(tmp_path, 'train_test.pkl'))]
