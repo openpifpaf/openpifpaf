960a1e1c4e89e8ab2e1f15e8dc21692b25235fdc
Duncan Zauss <Duncan.Zauss@gmx.net>
2021-03-03 09:27:29 +0100

    Cleaned up the test

--- tests/test_weighted_cif.py
+++ tests/test_weighted_cif.py
@@ -4,17 +4,15 @@ import torch
 import openpifpaf
 
 from openpifpaf.plugins.coco.constants import (
-    COCO_CATEGORIES,
     COCO_KEYPOINTS,
     COCO_PERSON_SKELETON,
     COCO_PERSON_SIGMAS,
     COCO_PERSON_SCORE_WEIGHTS,
     COCO_UPRIGHT_POSE,
-    DENSER_COCO_PERSON_CONNECTIONS,
-    HFLIP,
 )
 
 def test_conf_ones_weight():
+    # Weights = None
     cif = openpifpaf.headmeta.Cif('cif', 'cocokp',
                                       keypoints=COCO_KEYPOINTS,
                                       sigmas=COCO_PERSON_SIGMAS,
@@ -30,13 +28,14 @@ def test_conf_ones_weight():
     correct_values = np.array([0.024449902, 830.4298, 66.78698]) #computed with the original loss without weights
     np.testing.assert_allclose(loss_np, correct_values, atol=1e-7)
     
+    # Weights explicitly set to 1.0
     cif = openpifpaf.headmeta.Cif('cif', 'cocokp',
                                       keypoints=COCO_KEYPOINTS,
                                       sigmas=COCO_PERSON_SIGMAS,
                                       pose=COCO_UPRIGHT_POSE,
                                       draw_skeleton=COCO_PERSON_SKELETON,
                                       score_weights=COCO_PERSON_SCORE_WEIGHTS,
-                                      weights = [1]*17)
+                                      weights = [1.]*17)
     head_net = openpifpaf.network.heads.CompositeField3(cif, 1396)
     loss = openpifpaf.network.losses.composite.CompositeLoss(head_net)
     x = torch.ones((2,17,5,4,4,)) * 5
@@ -79,7 +78,8 @@ def test_conf_zero_weight():
     loss_values = loss(x, t)
     loss_np = np.array([l.numpy() for l in loss_values])
     np.testing.assert_allclose(loss_np, np.zeros((3)), atol=1e-7)
-    
-#test_conf_ones_weight()
-#test_conf_equal_weight()
-#test_conf_zero_weight()
+
+if __name__=="__main__":
+    test_conf_ones_weight()
+    test_conf_equal_weight()
+    test_conf_zero_weight()
