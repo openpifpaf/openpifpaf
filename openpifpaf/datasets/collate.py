3537e26660a53b9fdac952f82058567954c3c03e
Sven Kreiss <me@svenkreiss.com>
2021-05-06 14:45:54 +0200

    tracking collate

--- openpifpaf/datasets/collate.py
+++ openpifpaf/datasets/collate.py
@@ -13,3 +13,13 @@ def collate_images_targets_meta(batch):
     targets = torch.utils.data.dataloader.default_collate([b[1] for b in batch])
     metas = [b[2] for b in batch]
     return images, targets, metas
+
+
+def collate_tracking_images_targets_meta(batch):
+    images = torch.utils.data.dataloader.default_collate([
+        im for group in batch for im in group[0]])
+
+    targets = torch.utils.data.dataloader.default_collate([b[1] for b in batch])
+    metas = [b[2] for b in batch]
+
+    return images, targets, metas
