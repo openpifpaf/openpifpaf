5eb90f16f675f9758fc1930e0bb38772f50ca751
Lorenzo <lorenzobertoni9@gmail.com>
2021-05-12 18:35:55 +0200

    remove https

--- openpifpaf/plugins/animalpose/animalpose/__init__.py
+++ openpifpaf/plugins/animalpose/animalpose/__init__.py
@@ -6,5 +6,5 @@ from . import animal_kp
 def register():
     openpifpaf.DATAMODULES['animal'] = animal_kp.AnimalKp
     openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-animalpose'] = \
-        "https://github.com/vita-epfl/openpifpaf-torchhub/releases/" \
+        "http://github.com/vita-epfl/openpifpaf-torchhub/releases/" \
         "download/v0.12.9/shufflenetv2k30-210511-120906-animal.pkl.epoch400"
