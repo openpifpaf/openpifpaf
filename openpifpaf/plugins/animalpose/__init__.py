b43b366d7e18c5380a8541f5a98bcd7b5fa47a20
Lorenzo <lorenzobertoni9@gmail.com>
2021-05-14 14:50:48 +0200

    trailing-newlines

--- openpifpaf/plugins/animalpose/__init__.py
+++ openpifpaf/plugins/animalpose/__init__.py
@@ -9,4 +9,3 @@ def register():
     openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-animalpose'] = \
         "http://github.com/vita-epfl/openpifpaf-torchhub/releases/" \
         "download/v0.12.9/shufflenetv2k30-210511-120906-animal.pkl.epoch400"
-
