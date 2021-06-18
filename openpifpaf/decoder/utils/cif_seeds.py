6a5642efb3dcf53f153afb3ba57ac2882387c423
Sven Kreiss <me@svenkreiss.com>
2021-06-18 16:20:21 +0200

    adapt decoder to new field format

--- openpifpaf/decoder/utils/cif_seeds.py
+++ openpifpaf/decoder/utils/cif_seeds.py
@@ -88,11 +88,11 @@ class CifDetSeeds(CifSeeds):
 
         cif = all_fields[meta.head_index]
         for field_i, p in enumerate(cif):
-            p = p[:, p[0] > self.threshold]
+            p = p[:, p[1] > self.threshold]
             if meta.decoder_min_scale:
                 p = p[:, p[4] > meta.decoder_min_scale / meta.stride]
                 p = p[:, p[5] > meta.decoder_min_scale / meta.stride]
-            c, x, y, w, h, _, __ = p
+            _, c, x, y, w, h = p
 
             if self.ablation_no_rescore:
                 v = c
