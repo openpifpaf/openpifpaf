40ea79ff113cf0053c77a36bdb06ba712edcda5a
Duncan Zauss <Duncan.Zauss@gmx.net>
2021-07-05 11:53:08 +0200

    Added new checkpoints, added the new skeleton with connected knuckles, fixed documentation wholebodykp --> wholebody

--- openpifpaf/plugins/wholebody/__init__.py
+++ openpifpaf/plugins/wholebody/__init__.py
@@ -5,7 +5,7 @@ from .wholebody import Wholebody
 
 def register():
     openpifpaf.DATAMODULES['wholebody'] = Wholebody
-    openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-wholebody'] = 'http://github.com/DuncanZauss/' \
-        'openpifpaf_assets/releases/download/v0.1.0/wb_shufflenet16_mixed_foot.pkl.epoch550'
-    openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-wholebody'] = 'http://github.com/DuncanZauss/' \
-        'openpifpaf_assets/releases/download/v0.1.0/wb_shufflenet30_mixed_foot.pkl.epoch350'
+    openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-wholebody'] = 'https://github.com/DuncanZauss/' \
+        'openpifpaf_assets/releases/download/v0.1.0/sk16_wholebody.pkl'
+    openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-wholebody'] = 'https://github.com/DuncanZauss/' \
+        'openpifpaf_assets/releases/download/v0.1.0/sk30_wholebody.pkl'
