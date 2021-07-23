86faa9d1086514a0216f26781ccf6f4b7d7d27bf
Sven Kreiss <me@svenkreiss.com>
2021-07-23 17:31:42 +0200

    detect libtorch

--- tests/test_cmake.py
+++ tests/test_cmake.py
@@ -11,6 +11,11 @@ def test_build(tmpdir):
     assert not os.path.exists(tmpdir.join('openpifpaf-video'))
     assert not os.path.exists(tmpdir.join('libopenpifpafstatic.a'))
 
+    env = os.environ.copy()
+    if os.path.exists('libtorch'):
+        print('found libtorch in current directory')
+        env['Torch_DIR'] = os.getcwd().join(['libtorch', 'share', 'cmake', 'Torch'])
+
     cpp_folder = os.path.join(os.path.dirname(__file__), '..', 'cpp')
     configure_cmd = [
         'cmake',
@@ -18,7 +23,8 @@ def test_build(tmpdir):
         cpp_folder,
     ]
     print(configure_cmd)
-    subprocess.run(configure_cmd, cwd=tmpdir, check=True)
+    print(os.getcwd(), env)
+    subprocess.run(configure_cmd, cwd=tmpdir, check=True, env=env)
 
     build_cmd = [
         'cmake', '--build', '.', '-j4', '--config', 'Release', '--verbose'
