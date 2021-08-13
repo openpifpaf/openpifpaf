bff28fa68f8a06025e245d84be2c84f3eb142857
Duncan Zauss <duncan.zauss@gmx.net>
2021-08-13 09:11:32 +0200

    Added new apollo checkpoints

--- openpifpaf/plugins/apollocar3d/__init__.py
+++ openpifpaf/plugins/apollocar3d/__init__.py
@@ -10,4 +10,8 @@ def register():
         "download/v0.1.0/shufflenetv2k16-201113-135121-apollo.pkl.epoch290"
     openpifpaf.CHECKPOINT_URLS['shufflenetv2k16-apollo-66'] = \
         "http://github.com/DuncanZauss/openpifpaf_assets/releases/" \
-        "download/v0.1.0/apollo_66kp-7d6ccbb9.pkl"
+        "download/v0.1.0/sk16_apollo_66kp.pkl"
+    openpifpaf.CHECKPOINT_URLS['shufflenetv2k30-apollo-66'] = \
+        "http://github.com/DuncanZauss/openpifpaf_assets/releases/" \
+        "download/v0.1.0/sk30_apollo_66kp.pkl"
+
