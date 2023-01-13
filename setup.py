ed2cdb823ddbc9ae3db0025235e1dfc177a4e709
Sven Kreiss <me@svenkreiss.com>
2023-01-13 21:39:33 +0100

    torch 1.13.1, torchvision 0.14.1

--- setup.py
+++ setup.py
@@ -84,8 +84,8 @@ setuptools.setup(
         'numpy>=1.16,<1.24',
         'pysparkling',  # for log analysis
         'python-json-logger',
-        'torch==1.13.0',
-        'torchvision==0.14.0',
+        'torch==1.13.1',
+        'torchvision==0.14.1',
         'pillow!=8.3.0',  # exclusion torchvision 0.10.0 compatibility
         'dataclasses; python_version<"3.7"',
     ],
