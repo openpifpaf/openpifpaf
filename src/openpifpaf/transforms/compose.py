d3c9f03937ea228eca60effb7fe51f106568c079
Sven Kreiss <me@svenkreiss.com>
2023-01-21 17:11:24 +0100

    minor type fixes

--- src/openpifpaf/transforms/compose.py
+++ src/openpifpaf/transforms/compose.py
@@ -1,17 +1,18 @@
-from typing import List
+import typing as t
 
 from .preprocess import Preprocess
 
 
 class Compose(Preprocess):
     """Execute given transforms in sequential order."""
-    def __init__(self, preprocess_list: List[Preprocess]):
+    def __init__(self, preprocess_list: t.List[t.Optional[Preprocess]]):
         self.preprocess_list = preprocess_list
 
     def __call__(self, *args):
         for p in self.preprocess_list:
             if p is None:
                 continue
+            assert args is not None
             args = p(*args)
 
         return args
