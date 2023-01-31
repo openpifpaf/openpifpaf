b9de0d708b578751060af03c6ccad2e101b1a6cf
Sven Kreiss <me@svenkreiss.com>
2023-01-31 23:46:46 +0100

    fix test

--- tests/test_scale_loss.py
+++ tests/test_scale_loss.py
@@ -5,23 +5,23 @@ import openpifpaf
 
 
 def test_relative_scale_loss():
-    loss = openpifpaf.network.losses.components.Scale()
-    x = torch.ones((4,))
+    loss = openpifpaf.network.losses.components.Scale([0], [0])
+    x = torch.ones((1, 1, 1, 4, 1))
     x = torch.log(torch.exp(x) - 1)  # inverse softplus
-    t = torch.ones((4,))
+    t = torch.ones((1, 1, 1, 4, 1))
+
     loss_values = loss(x, t)
     np.testing.assert_allclose(loss_values.numpy(), [0, 0, 0, 0], atol=1e-7)
 
 
 def test_relative_scale_loss_masked():
-    loss = openpifpaf.network.losses.components.Scale()
-    x = torch.ones((4,))
+    loss = openpifpaf.network.losses.components.Scale([0], [0])
+    x = torch.ones((1, 1, 1, 4, 1))
     x = torch.log(torch.exp(x) - 1)  # inverse softplus
-    t = torch.ones((4,))
+    t = torch.ones((1, 1, 1, 4, 1))
 
-    mask = x > 1.0
-    x = torch.masked_select(x, mask)
-    t = torch.masked_select(t, mask)
+    mask = x > 0.5  # select none
+    t[mask] = np.nan
 
     loss_values = loss(x, t)
     assert loss_values.sum().numpy() == 0.0
